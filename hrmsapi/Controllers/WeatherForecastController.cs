96312c3645528979bada8ee6bfdad7e57bf00437
rishank02121993 <rishank02121993@gmail.com>
2025-01-19 16:32:28 +0530

    Trying to commit

--- hrmsapi/Controllers/WeatherForecastController.cs
+++ hrmsapi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace hrmsapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "1"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
